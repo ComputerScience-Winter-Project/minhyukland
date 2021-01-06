3744e7f219c18bb5efc902cc0ac8ea7b6fab9807
조민혁 <alsgur2348@soongsil.ac.kr>
2021-01-07 01:59:13 +0900

    수정본

--- Tutorial2/src/lee.java
+++ Tutorial2/src/lee.java
@@ -1,9 +1,12 @@
-
+import java.util.Scanner;
 public class lee {
-
+		
 	public static void main(String[] args) {
 
 		System.out.println("æ»≥Á ¿Ãº±»£");
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		System.out.println(a);
+		
 	}
-
 }
